cc44373b843f29f3586051add15a51d3137748b0
Wayne del Corral Jr <wdelcorjr@gmail.com>
2021-03-05 03:07:45 -0600

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 const fi = (function() {
   return {
-    libraryMethod: function() {
-      return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
-    },
-
     each: function(collection, callback) {
       for (const [item, value] of Object.entries(collection)) {
         callback(value);
